8363b5cbf161ccf7fff5cd7ef8ab67fcd0adb09b
Everton Mello <everton.mello.281@gmail.com>
2022-07-27 22:15:29 -0300

    Exercício feito

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+const arrayNumbers = [3, 9, 4, 7, 2, 6, 5];
+console.log(arrayNumbers);
+console.log("------------------------------------------------");
+
+const arrayStrings = ["Everton", "Lima", "Mello", "Melani", "Oliveira"];
+console.log(arrayStrings);
+console.log("------------------------------------------------");
+
+const arrayMisto = [7, "Ton", true, "Mel", false, 5];
+console.log(arrayMisto);
+console.log("------------------------------------------------");
+
+
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,21 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const arrayNumbers = [3, 9, 4, 7, 2, 6, 5];
+console.log(arrayNumbers);
+const arrayStrings = ["Everton", "Lima", "Mello", "Melani", "Oliveira"];
+console.log(arrayStrings);
+const arrayMisto = [7, "Ton", true, "Mel", false, 5];
+console.log(arrayMisto);
+console.log("------------------------------------------------");
+
+console.log("Quantidade de ítens do primeiro array: ", arrayNumbers.length);
+console.log("Quantidade de ítens do segundo array: ", arrayStrings.length);
+console.log("Quantidade de ítens do terceiro array: ", arrayMisto.length);
+console.log("------------------------------------------------");
+
+console.log("Primeiro ítem do primeiro array: ", arrayNumbers[0]);
+console.log("Segundo ítem do segundo array: ", arrayStrings[1]);
+console.log("Terceiro ítem do terceiro array: ", arrayMisto[2]);
+console.log("------------------------------------------------");
+
+console.log("O número 5 está incluído no primeior array? ", arrayNumbers.includes(5));
+console.log("O número 8 está incluído no terceiro array? ", arrayMisto.includes(8));
+console.log("------------------------------------------------");
